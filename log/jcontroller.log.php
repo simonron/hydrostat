275e558833a3d35e1601eb26fcd8c210e98bfda6
simonron <Cyberman@Cyberbug.local>
2014-08-19 15:13:02 +1000

    template fix attempt
    Trying to update the template from the server

---
+++ log/jcontroller.log.php
@@ -0,0 +1,34 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-19 04:48:42 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-08-19T04:48:42+00:00	INFO	controller	Holding edit ID com_templates.edit.style.27 Array (     [0] => 27 ) 
+2014-08-19T04:48:42+00:00	INFO	controller	Checking edit ID com_templates.edit.style.27: 1 Array (     [0] => 27 ) 
+2014-08-19T04:48:55+00:00	INFO	controller	Holding edit ID com_templates.edit.style.23 Array (     [0] => 27     [1] => 23 ) 
+2014-08-19T04:48:55+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [0] => 27     [1] => 23 ) 
+2014-08-19T04:49:12+00:00	INFO	controller	Holding edit ID com_templates.edit.style.21 Array (     [0] => 27     [1] => 23     [2] => 21 ) 
+2014-08-19T04:49:12+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [0] => 27     [1] => 23     [2] => 21 ) 
+2014-08-19T04:49:34+00:00	INFO	controller	Holding edit ID com_templates.edit.style.23 Array (     [0] => 27     [1] => 23     [2] => 21 ) 
+2014-08-19T04:49:34+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [0] => 27     [1] => 23     [2] => 21 ) 
+2014-08-19T04:54:43+00:00	INFO	controller	Releasing edit ID com_templates.edit.style.23 Array (     [0] => 27     [2] => 21 ) 
+2014-08-19T04:56:32+00:00	INFO	controller	Holding edit ID com_templates.edit.style.23 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:56:32+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:56:58+00:00	INFO	controller	Holding edit ID com_templates.edit.style.21 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:56:58+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:57:39+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:57:51+00:00	INFO	controller	Holding edit ID com_templates.edit.style.27 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:57:51+00:00	INFO	controller	Checking edit ID com_templates.edit.style.27: 1 Array (     [0] => 27     [2] => 21     [3] => 23 ) 
+2014-08-19T04:58:10+00:00	INFO	controller	Releasing edit ID com_templates.edit.style.27 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T04:58:23+00:00	INFO	controller	Holding edit ID com_templates.edit.style.23 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T04:58:23+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T04:59:12+00:00	INFO	controller	Releasing edit ID com_templates.edit.style.23 Array (     [2] => 21 ) 
+2014-08-19T05:00:59+00:00	INFO	controller	Holding edit ID com_templates.edit.style.23 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:00:59+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:01:31+00:00	INFO	controller	Checking edit ID com_templates.edit.style.23: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:06:24+00:00	INFO	controller	Holding edit ID com_templates.edit.style.21 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:06:24+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:06:28+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:07:49+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [2] => 21     [3] => 23 ) 
+2014-08-19T05:09:12+00:00	INFO	controller	Checking edit ID com_templates.edit.style.21: 1 Array (     [2] => 21     [3] => 23 ) 
--- t3-assets/dev/t3/02fb4912ac9e612fe791b57dec6df942-cache-t3-debe22c516ecd93a700d01df70ea0d7d.php
+++ t3-assets/dev/t3/02fb4912ac9e612fe791b57dec6df942-cache-t3-debe22c516ecd93a700d01df70ea0d7d.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#i:1408411421;
+<?php die("Access Denied"); ?>#x#i:1408423443;
